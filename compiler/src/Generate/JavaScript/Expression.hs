5b75f484571474f329eb92cbebefa013dd5691f2
Jeroen Engels <jfm.engels@gmail.com>
2024-06-25 17:41:24 +0200

    Restore decomposition code

--- compiler/src/Generate/JavaScript/Expression.hs
+++ compiler/src/Generate/JavaScript/Expression.hs
@@ -516,6 +516,9 @@ generateBasicsCall mode home name args =
       case name of
         -- NOTE: removed "composeL" and "composeR" because of this issue:
         -- https://github.com/elm/compiler/issues/1722
+        -- NOTE: Restored in Lamdera
+        "composeL" -> decomposeL mode elmLeft [elmRight]
+        "composeR" -> decomposeR mode [elmLeft] elmRight
         "append"   -> append mode elmLeft elmRight
         "apL"      -> generateJsExpr mode $ apply elmLeft elmRight
         "apR"      -> generateJsExpr mode $ apply elmRight elmLeft
@@ -544,6 +547,30 @@ generateBasicsCall mode home name args =
 
     _ ->
       generateGlobalCall home name (map (generateJsExpr mode) args)
+
+
+decomposeL :: Mode.Mode -> Opt.Expr -> [Opt.Expr] -> JS.Expr
+decomposeL mode expr funcs =
+  case expr of
+    Opt.Call (Opt.VarGlobal (Opt.Global home "composeL")) [left, func]
+      | home == ModuleName.basics ->
+          decomposeL mode left (func:funcs)
+
+    _ ->
+      generateJsExpr mode $
+        Opt.Function [Name.dollar] (foldr apply (Opt.VarLocal Name.dollar) (expr:funcs))
+
+
+decomposeR :: Mode.Mode -> [Opt.Expr] -> Opt.Expr -> JS.Expr
+decomposeR mode funcs expr =
+  case expr of
+    Opt.Call (Opt.VarGlobal (Opt.Global home "composeR")) [func, right]
+      | home == ModuleName.basics ->
+          decomposeR mode (func:funcs) right
+
+    _ ->
+      generateJsExpr mode $
+        Opt.Function [Name.dollar] (foldr apply (Opt.VarLocal Name.dollar) (expr:funcs))
 
 
 equal :: JS.Expr -> JS.Expr -> JS.Expr
