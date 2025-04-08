b1962b39555c4d90dd77e129c23b3981fe89ab9d
Jeroen Engels <jfm.engels@gmail.com>
2025-04-08 17:29:30 +0200

    Add WASM entrypoint to compile

---
+++ extra/Typing/WasmMain.hs
@@ -0,0 +1,38 @@
+module WasmMain where
+
+import Data.ByteString.Builder qualified
+import Data.ByteString.Lazy qualified
+import Data.ByteString.Lazy.UTF8 qualified -- from utf8-string
+import Data.ByteString.UTF8 qualified -- from utf8-string
+import Debug.Trace
+import GHC.Wasm.Prim qualified as Wasm -- See https://gitlab.haskell.org/ghc/ghc/-/commit/317a915bc46fee2c824d595b0d618057bf7fbbf1#82b5a034883a3ede9540d6423738da627660f860
+import Json.Encode ((==>))
+import Json.Encode qualified
+import ToStringHelper
+import Ulm.Repl qualified
+
+main :: IO ()
+main = mempty
+
+foreign export javascript "check"
+  checkJs :: Wasm.JSString -> IO Wasm.JSString
+
+checkJs :: Wasm.JSString -> IO Wasm.JSString
+checkJs jsString =
+  fmap encodeJson $
+    Json.Encode.object
+      [ "type" ==> Json.Encode.chars "some-type"
+      , "name" ==> Json.Encode.chars "some-name"
+      ]
+
+encodeJson :: Json.Encode.Value -> Wasm.JSString
+encodeJson value =
+  builderToJsString $ Json.Encode.encode value
+
+builderToJsString :: Data.ByteString.Builder.Builder -> Wasm.JSString
+builderToJsString builder =
+  let lazyStr :: Data.ByteString.Lazy.LazyByteString
+      lazyStr = Data.ByteString.Builder.toLazyByteString builder
+      str :: String
+      str = Data.ByteString.Lazy.UTF8.toString lazyStr
+   in Wasm.toJSString str
